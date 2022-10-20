a8d5ceb1c05a88e48c633bdeb586f7dc4d189f1c
hoang4963 <hoangth.finbox@gmail.com>
2022-10-21 00:02:13 +0700

    a

---
+++ src/main/java/com/codegym/cms/repository/customer/ICustomerRepository.java
@@ -0,0 +1,7 @@
+package com.codegym.cms.repository.customer;
+
+import com.codegym.cms.model.Customer;
+import com.codegym.cms.repository.IGeneralRepository;
+
+public interface ICustomerRepository extends IGeneralRepository<Customer> {
+}
